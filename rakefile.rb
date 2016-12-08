83d581b43f7e1d7ce2e3ca3f9c1889cfb92ff4b5
Michael Sevestre <michael.sevestre@gmail.com>
2016-12-08 17:05:03 -0500

    Update setup to use zip versions of components

--- rakefile.rb
+++ rakefile.rb
@@ -5,30 +5,38 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 task :default => [:load_dependencies, :setup]
 
-VERSION = ENV['APPVEYOR_BUILD_VERSION']
+VERSION = '0.4.2'
+FULL_VERSION = ENV['APPVEYOR_BUILD_VERSION'] || '0.4.2.0'
 PRODUCT_NAME = 'ConsoleAppBundle'
-REPLACEMENTS = {}
+GITHUB_NAME = 'msevestre'
+
+#Account name on AppVeyor
 ACCOUNT_NAME = 'MichaelSevestre'
 
+#Branch used to retrieve custom msi packages
+BRANCH_NAME = 'develop'
+
+REPLACEMENTS = {}
+
+def create_package(appveyor_project_name, artifact_name, git_repository, version: VERSION, branch:BRANCH_NAME)
+  compressed = artifact_name.include? '.zip'
+  return {
+    appveyor_project_name: appveyor_project_name,
+    artifact_name: artifact_name,
+    branch: branch,
+    compressed: compressed,
+    git_repository: git_repository,
+    version: version
+  } 
+end
+
 MSI = {
-  'CONSOLE_APP'=> 
-    {
-      'project_name'=> 'consoleapp-yvmia',
-      'artifact_name' =>'ConsoleApp.Setup.msi',
-      'artifact_path'=> 'setup/ConsoleApp.Setup/bin/Setup/',
-      'branch'=>  'develop'
-    },
-  'MIKTEX'=>
-    {
-      'project_name'=> 'miktex',
-      'artifact_name' =>'MikTex.2.9.2.9711.msi',
-      'artifact_path' => '',
-      'branch' => 'master'
-    },
+  'CONSOLE_APP'=>  create_package('consoleapp-yvmia', 'setup.zip', 'consoleapp'),
+  'MIKTEX'=> create_package('miktex', 'MikTex.2.9.2.9711.msi','miktex', version: '2.9.2', branch: 'master'),
   'DOTNET'=>
     {
-      'artifact_name' => 'dotnetfx45_full_x86_x64.exe',
-      'uri' => 'http://go.microsoft.com/fwlink/?LinkId=225702'
+      artifact_name: 'dotnetfx45_full_x86_x64.exe',
+      uri: 'http://go.microsoft.com/fwlink/?LinkId=225702'
     }
   }   
 
@@ -41,19 +49,16 @@ end
 desc "Create suite setup"
 task :setup => :fetch do
   REPLACEMENTS['PRODUCT_FULL_NAME'] =  PRODUCT_NAME
-  REPLACEMENTS['PRODUCT_FULL_VERSION'] =  VERSION
+  REPLACEMENTS['PRODUCT_FULL_VERSION'] =  FULL_VERSION
   
   copy "bundle.wxs", deploy_dir
-#  copy_setup_dependencies
   
-  version_file = File.join(deploy_dir,'versions.txt')
+  version_file = File.join(output_dir,'versions.txt')
   File.open(version_file, 'w') do |file| 
     REPLACEMENTS.each do |k,v|
       file.puts "#{k}: #{v}"
     end    
   end 
-
-  #copy_to_daily_build version_file
 
  Utils.replace_tokens REPLACEMENTS, File.join(deploy_dir, "bundle.wxs")
 
@@ -74,7 +79,7 @@ def run_light(exe)
 end
 
 def create_setup(compressed, name)
-  exe = "#{output_dir}/#{name}.#{VERSION}.exe"
+  exe = "#{output_dir}/#{name}.#{FULL_VERSION}.exe"
   run_candle compressed
   run_light exe
 end
@@ -100,29 +105,66 @@ end
 def prepare_msi(msi)
   package =  MSI[msi]
   file = download package
-
-  REPLACEMENTS[msi] = package['artifact_name'] 
-  #  copy_to_daily_build file
+  puts file
+  package_name = retrieve_package_name(file, package) 
+  download_path = "https://github.com/#{GITHUB_NAME}/#{package[:git_repository]}/releases/download/v#{package[:version]}/#{package_name}"
+  REPLACEMENTS["#{msi}_DOWNLOAD_PATH"] = download_path
+  REPLACEMENTS[msi] = package_name
 end
 
 def download(package)
-  file_name = package['artifact_name'];
-  file = File.join(deploy_dir,file_name)
-  uri = package['uri'];
-  uri = "https://ci.appveyor.com/api/projects/#{ACCOUNT_NAME}/#{package['project_name']}/artifacts/#{package['artifact_path']}#{file_name}?branch=#{package['branch']}" unless uri
+  file_name = package[:artifact_name]
+  uri = package[:uri];
+  uri = "https://ci.appveyor.com/api/projects/#{ACCOUNT_NAME}/#{package[:appveyor_project_name]}/artifacts/#{package[:artifact_path]}#{file_name}?branch=#{package[:branch]}" unless uri
+  download_file file_name, uri
+end
 
-  puts "Download #{file_name} from #{uri}"
+def download_file(file_name, uri)
+  file = File.join(deploy_dir, file_name)
+  puts "Downloading #{file_name} from #{uri}"
   open(file, 'wb') do |fo| 
     fo.print open(uri,:read_timeout => nil).read
   end
   file
 end
 
+def retrieve_package_name(package_full_path, package)
+  compressed = package[:compressed] || false
+  artifact_name = package[:artifact_name];
+  #pointing to real package already return 
+  return artifact_name unless compressed
+
+  unzip_dir = unzip(package_full_path)
+  copy_msi_to_deploy unzip_dir  
+end
+
+#copy all msi packages defined under dir and return the name of the packages found (should only be one)
+def copy_msi_to_deploy(dir)
+  artifact_name = ''
+  Dir.glob(File.join(dir, '*.msi')) do |x|
+    copy x, deploy_dir
+    artifact_name = File.basename(x)
+  end 
+  artifact_name
+end
+
+def unzip(package_full_path)
+  zip_name = File.basename(package_full_path, '.zip')
+  unzip_dir = File.join(deploy_dir, zip_name)
+  command_line = %W[e #{package_full_path} -o#{unzip_dir}]
+  Utils.run_cmd('7z', command_line)
+  unzip_dir
+end
+
 def deploy_dir
-  File.join(File.join(File.dirname(__FILE__),'deploy'))
+  File.join(current_dir,'deploy')
 end
 
 def output_dir
-  File.join(File.join(File.dirname(__FILE__),'output'))
+  File.join(current_dir,'output')
+end
+
+def current_dir
+  File.dirname(__FILE__)
 end
 
